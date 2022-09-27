9560a0c6164a132d2909ccacea1215973fa4fb1c
digiDreams <digital2dreamer@gmail.com>
2022-09-27 16:29:01 +0200

    First commit: adding all project at start

---
+++ franceLocations/Data/dataModel.swift
@@ -0,0 +1,8 @@
+//
+//  dataModel.swift
+//  franceLocations
+//
+//  Created by Apprenant 16 on 27/09/2022.
+//
+
+import Foundation
---
+++ franceLocations/franceLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  franceLocationsApp.swift
+//  franceLocations
+//
+//  Created by Apprenant 16 on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct franceLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
